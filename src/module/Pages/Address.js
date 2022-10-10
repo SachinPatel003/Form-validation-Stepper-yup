035f4402c9d1aee64ba7c3db9db76f5bf6800645
Sachin <sachinpatel460@gmail.com>
2022-10-10 16:16:12 +0530

    Fourth commit

--- src/module/Pages/Address.js
+++ src/module/Pages/Address.js
@@ -7,7 +7,6 @@ const Aquisition = ({ control }) => {
     <>
       <FormSelect control={control} name="state" label="State" helperText="Please select your state" m1="Gujrat"  m2="Rajasthan"  />
       <FormSelect control={control} name="city" label="City" helperText="Please select your city" m1="Ahmedabad" m2="Jaipur" />
-x
     </>
   );
 };
